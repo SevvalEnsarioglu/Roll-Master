46632fbde62b54907e270a0754554407b2beddbd
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2024-03-28 12:12:22 +0300

    Initial Commit

---
+++ Roll Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll Master
+//
+//  Created by Şevval Ensarioğlu on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Roll MasterUITests/Roll_MasterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Roll_MasterUITestsLaunchTests.swift
+//  Roll MasterUITests
+//
+//  Created by Şevval Ensarioğlu on 28.03.2024.
+//
+
+import XCTest
+
+final class Roll_MasterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
